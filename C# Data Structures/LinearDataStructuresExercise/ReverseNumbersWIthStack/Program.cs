8ac11a982ebdab1ff95345347740ee600d2c571c
WIKSATA1337 <ciganskigeigerwiksa26g@gmail.com>
2022-11-14 22:23:30 +0200

    Added one exercise.

---
+++ C# Data Structures/LinearDataStructuresExercise/ReverseNumbersWIthStack/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> reversed = new Stack<int>();
+
+// Automatic way
+// Stack<int> reversed = new Stack<int>(inputNumbers);
+
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+    reversed.Push(inputNumbers[i]);
+}
+
+Console.WriteLine(string.Join(" ", reversed));
