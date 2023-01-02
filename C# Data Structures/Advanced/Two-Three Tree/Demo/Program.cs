84272db90403b3f84d8cc0d6269cafa66b2ffc5e
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-01-02 20:40:35 +0200

    2-3 Tree and Red-Black Tree Implementation

---
+++ C# Data Structures/Advanced/Two-Three Tree/Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using _01.Two_Three;
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] arr = { "F", "C", "G", "A", "B", "D", "E", "K", "I", "G", "H", "J", "K" };
+            var tree = new TwoThreeTree<string>();
+            foreach (var item in arr)
+            {
+                tree.Insert(item);
+            }
+
+            Console.WriteLine(tree.ToString());
+        }
+    }
+}
