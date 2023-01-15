c3dafbf3c1274e991d54cfe2847c9d71c93e651c
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-01-15 17:51:45 +0200

    3-bit Vectors Exercise.

---
+++ C# Algorithms/RecursionAndBacktracking/RecursionAndBacktracking/ThreeBitVectors.cs
@@ -0,0 +1,28 @@
+﻿namespace RecursionAndBacktracking
+{
+    public static class ThreeBitVectors
+    {
+        public static void Run(int vectorLength)
+        {
+            int[] arr = new int[vectorLength];
+
+            Solve(arr, 0);
+        }
+
+        private static void Solve(int[] arr, int idx)
+        {
+            if (idx >= arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                arr[idx] = i;
+
+                Solve(arr, idx + 1);
+            }
+        }
+    }
+}
