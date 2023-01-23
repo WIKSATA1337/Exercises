bdba94fe6a5943c8ef2b4654328253b5f003f899
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-01-23 17:18:03 +0200

    Dynamic Programming Exercises

---
+++ Algorithms/Fundamentals/DynamicProgrammingExercises/DynamicProgrammingExercises/BinomialCoefficients.cs
@@ -0,0 +1,36 @@
+﻿namespace DynamicProgrammingExercises
+{
+    public static class BinomialCoefficients
+    {
+        private static Dictionary<string, long> cache;
+
+        public static void Run()
+        {
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+
+            cache = new Dictionary<string, long>();
+
+            Console.WriteLine(BC(row, col));
+        }
+
+        private static long BC(int row, int col)
+        {
+            if (col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            var key = $"{row}-{col}";
+
+            if (cache.ContainsKey(key))
+            {
+                return cache[key];
+            }
+
+            cache[key] = BC(row - 1, col - 1) + BC(row - 1, col);
+
+            return cache[key];
+        }
+    }
+}
---
+++ Algorithms/Fundamentals/DynamicProgrammingExercises/DynamicProgrammingExercises/DividingPresents.cs
@@ -0,0 +1,56 @@
+﻿namespace DynamicProgrammingExercises
+{
+    public static class DividingPresents
+    {
+        public static void Run()
+        {
+            var presents = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var allSums = FindAllSums(presents);
+
+            var totalSum = presents.Sum();
+            var firstGuySum = totalSum / 2;
+
+            while (true)
+            {
+                if (allSums.ContainsKey(firstGuySum))
+                {
+                    break;
+                }
+
+                firstGuySum--;
+            }
+
+            Console.WriteLine($"Difference: {Math.Max(firstGuySum, totalSum - firstGuySum) - Math.Min(totalSum - firstGuySum, firstGuySum)}");
+
+            Console.WriteLine($"Alan: {firstGuySum} Bob: {totalSum - firstGuySum}");
+        }
+
+        private static Dictionary<int, int> FindAllSums(int[] elements)
+        {
+            var sums = new Dictionary<int, int>() { { 0, 0 } };
+
+            foreach (var element in elements)
+            {
+                var currSums = sums.Keys.ToArray();
+
+                foreach (var sum in currSums)
+                {
+                    var newSum = sum + element;
+
+                    if (sums.ContainsKey(newSum))
+                    {
+                        continue;
+                    }
+
+                    sums.Add(newSum, element);
+                }
+            }
+
+            return sums;
+        }
+    }
+}
