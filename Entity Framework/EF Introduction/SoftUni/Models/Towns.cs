3f9fd5d564bf7477bb5880cfc2ab3a2246adbc12
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-02-15 10:53:59 +0200

    EF Course materials added.

---
+++ Entity Framework/EF Introduction/SoftUni/Models/Towns.cs
@@ -0,0 +1,15 @@
+﻿namespace SoftUni.Models
+{
+    public partial class Towns
+    {
+        public Towns()
+        {
+            Addresses = new HashSet<Addresses>();
+        }
+
+        public int TownId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Addresses> Addresses { get; set; }
+    }
+}
---
+++ Entity Framework/EF Introduction/SoftUni/Program.cs
@@ -0,0 +1,14 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SoftUniContext dbContext = new SoftUniContext())
+            {
+            }
+        }
+    }
+}
