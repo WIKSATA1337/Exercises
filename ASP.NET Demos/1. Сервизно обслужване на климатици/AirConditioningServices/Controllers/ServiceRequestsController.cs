1cf5b81eb28b89945af6b782fe5680960557e5f1
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-03-16 08:35:52 +0200

    Demo 1 fixed.

--- ASP.NET Demos/1. Сервизно обслужване на климатици/AirConditioningServices/Controllers/ServiceRequestsController.cs
+++ ASP.NET Demos/1. Сервизно обслужване на климатици/AirConditioningServices/Controllers/ServiceRequestsController.cs
@@ -7,207 +7,240 @@ using AirConditioningServices.Data.Models.Enums;
 
 namespace AirConditioningServices.Controllers
 {
-    [Authorize]
-    public class ServiceRequestsController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+	[Authorize]
+	public class ServiceRequestsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
 
-        public ServiceRequestsController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		public ServiceRequestsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var currentUser = _context.Users
-                .First(u => u.UserName == User.Identity.Name);
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var currentUser = _context.Users
+				.First(u => u.UserName == User.Identity.Name);
 
-            var currentUserRequests = await _context.ServiceRequests
-                .Where(sr => sr.CreatedById == currentUser.Id)
-                .ToListAsync();
+			List<ServiceRequest>? currentUserRequests = null;
 
-            return View(currentUserRequests);
-        }
+			if (User.IsInRole("User"))
+			{
+				currentUserRequests = await _context.ServiceRequests
+					.Where(sr => sr.CreatedById == currentUser.Id)
+					.ToListAsync();
+			}
 
-        [HttpGet]
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null || _context.ServiceRequests == null)
-            {
-                return NotFound();
-            }
+			if (User.IsInRole("Tech"))
+			{
+				currentUserRequests = await _context.ServiceRequests
+					.Where(sr => sr.TechResponsible == null)
+					.ToListAsync();
+			}
 
-            var serviceRequest = await _context.ServiceRequests
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (serviceRequest == null)
-            {
-                return NotFound();
-            }
+			if (User.IsInRole("Administrator"))
+			{
+				return RedirectToPage("/Administrator/Index");
+			}
 
-            return View(serviceRequest);
-        }
+			return View(currentUserRequests);
+		}
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+		[HttpGet]
+		public async Task<IActionResult> Details(string id)
+		{
+			if (id == null || _context.ServiceRequests == null)
+			{
+				return NotFound();
+			}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,Address,Image,Status,VisitedDate,CreatedById")] ServiceRequest serviceRequest)
-        {
-            var currentUser = _context.Users
-                .First(u => u.UserName == User.Identity.Name);
+			var serviceRequest = await _context.ServiceRequests
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (serviceRequest == null)
+			{
+				return NotFound();
+			}
 
-            serviceRequest.Id = Guid.NewGuid().ToString();
-            serviceRequest.Status = ServiceStatuses.Waiting;
-            serviceRequest.VisitedDate = null;
-            serviceRequest.CreatedById = currentUser.Id;
-            serviceRequest.Image = new byte[1];
+			return View(serviceRequest);
+		}
 
-            ModelState.Remove("Image");
-            ModelState.Remove("VisitedDate");
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-            if (ModelState.IsValid)
-            {
-                _context.Add(serviceRequest);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> TakeRequest(ServiceRequest service)
+		{
+			var currentUser = _context.Users
+				.First(u => u.UserName == User.Identity.Name);
 
-            return View(serviceRequest);
-        }
+			service.Status = ServiceStatuses.InProgress;
+			service.TechResponsible = currentUser;
 
-        [HttpGet]
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null || _context.ServiceRequests == null)
-            {
-                return NotFound();
-            }
+			_context.Update(service);
+			await _context.SaveChangesAsync();
 
-            var serviceRequest = await _context.ServiceRequests.FindAsync(id);
+			return RedirectToAction(nameof(Index));
+		}
 
-            if (serviceRequest == null)
-            {
-                return NotFound();
-            }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name,Description,Address,Image,Status,VisitedDate,CreatedById")] ServiceRequest serviceRequest)
+		{
+			var currentUser = _context.Users
+				.First(u => u.UserName == User.Identity.Name);
 
-            var currentUser = _context.Users
-                .First(u => u.UserName == User.Identity.Name);
+			serviceRequest.Id = Guid.NewGuid().ToString();
+			serviceRequest.Status = ServiceStatuses.Waiting;
+			serviceRequest.VisitedDate = null;
+			serviceRequest.CreatedById = currentUser.Id;
+			serviceRequest.Image = new byte[1];
 
-            if (serviceRequest.CreatedById != currentUser.Id)
-            {
-                return NotFound();
-            }
+			ModelState.Remove("Image");
+			ModelState.Remove("VisitedDate");
 
-            return View(serviceRequest);
-        }
+			if (ModelState.IsValid)
+			{
+				_context.Add(serviceRequest);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Description,Address,Image,Status,VisitedDate,CreatedById")] ServiceRequest serviceRequest)
-        {
-            if (id != serviceRequest.Id)
-            {
-                return NotFound();
-            }
+			return View(serviceRequest);
+		}
 
-            serviceRequest.Status = ServiceStatuses.Waiting;
-            serviceRequest.VisitedDate = null;
-            serviceRequest.Image = new byte[1];
+		[HttpGet]
+		public async Task<IActionResult> Edit(string id)
+		{
+			if (id == null || _context.ServiceRequests == null)
+			{
+				return NotFound();
+			}
 
-            ModelState.Remove("Image");
-            ModelState.Remove("VisitedDate");
+			var serviceRequest = await _context.ServiceRequests.FindAsync(id);
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(serviceRequest);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ServiceRequestExists(serviceRequest.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(serviceRequest);
-        }
+			if (serviceRequest == null)
+			{
+				return NotFound();
+			}
 
-        [HttpGet]
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null || _context.ServiceRequests == null)
-            {
-                return NotFound();
-            }
+			var currentUser = _context.Users
+				.First(u => u.UserName == User.Identity.Name);
 
-            var serviceRequest = await _context.ServiceRequests
-                .FirstOrDefaultAsync(m => m.Id == id);
+			if (serviceRequest.CreatedById != currentUser.Id)
+			{
+				return NotFound();
+			}
 
-            if (serviceRequest == null)
-            {
-                return NotFound();
-            }
+			return View(serviceRequest);
+		}
 
-            if (serviceRequest.Status != ServiceStatuses.Waiting)
-            {
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Description,Address,Image,Status,VisitedDate,CreatedById")] ServiceRequest serviceRequest)
+		{
+			if (id != serviceRequest.Id)
+			{
+				return NotFound();
+			}
+
+			serviceRequest.Status = ServiceStatuses.Waiting;
+			serviceRequest.VisitedDate = null;
+			serviceRequest.Image = new byte[1];
+
+			ModelState.Remove("Image");
+			ModelState.Remove("VisitedDate");
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(serviceRequest);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ServiceRequestExists(serviceRequest.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(serviceRequest);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Delete(string id)
+		{
+			if (id == null || _context.ServiceRequests == null)
+			{
+				return NotFound();
+			}
+
+			var serviceRequest = await _context.ServiceRequests
+				.FirstOrDefaultAsync(m => m.Id == id);
+
+			if (serviceRequest == null)
+			{
+				return NotFound();
+			}
+
+			if (serviceRequest.Status != ServiceStatuses.Waiting)
+			{
 				return RedirectToPage("/ServiceRequests/Index");
 			}
 
-            var currentUser = _context.Users
-                .First(u => u.UserName == User.Identity.Name);
+			var currentUser = _context.Users
+				.First(u => u.UserName == User.Identity.Name);
 
-            if (serviceRequest.CreatedById != currentUser.Id)
-            {
-                return NotFound();
-            }
+			if (serviceRequest.CreatedById != currentUser.Id)
+			{
+				return NotFound();
+			}
 
-            return View(serviceRequest);
-        }
+			return View(serviceRequest);
+		}
 
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            if (_context.ServiceRequests == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.ServiceRequests'  is null.");
-            }
-            var serviceRequest = await _context.ServiceRequests.FindAsync(id);
-            if (serviceRequest == null)
-            {
-                return NotFound();
-            }
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(string id)
+		{
+			if (_context.ServiceRequests == null)
+			{
+				return Problem("Entity set 'ApplicationDbContext.ServiceRequests'  is null.");
+			}
+			var serviceRequest = await _context.ServiceRequests.FindAsync(id);
+			if (serviceRequest == null)
+			{
+				return NotFound();
+			}
 
-            var currentUser = _context.Users
-                .First(u => u.UserName == User.Identity.Name);
+			var currentUser = _context.Users
+				.First(u => u.UserName == User.Identity.Name);
 
-            if (serviceRequest.CreatedById != currentUser.Id)
-            {
-                return NotFound();
-            }
+			if (serviceRequest.CreatedById != currentUser.Id)
+			{
+				return NotFound();
+			}
 
-            _context.ServiceRequests.Remove(serviceRequest);
-            await _context.SaveChangesAsync();
+			_context.ServiceRequests.Remove(serviceRequest);
+			await _context.SaveChangesAsync();
 
-            return RedirectToAction(nameof(Index));
-        }
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool ServiceRequestExists(string id)
-        {
-            return (_context.ServiceRequests?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+		private bool ServiceRequestExists(string id)
+		{
+			return (_context.ServiceRequests?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
 }
