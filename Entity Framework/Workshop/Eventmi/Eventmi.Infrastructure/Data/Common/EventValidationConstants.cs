574b297d810ebaf2cf4f721a542229a0399200e8
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-03-21 20:43:43 +0200

    Workshop: Basic skeleton for Eventmi added.

---
+++ Entity Framework/Workshop/Eventmi/Eventmi.Infrastructure/Data/Common/EventValidationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Eventmi.Infrastructure.Data.Common
+{
+	public static class EventValidationConstants
+	{
+		public const int EventNameLength = 100;
+		public const int EventPlaceLength = 60;
+	}
+}
