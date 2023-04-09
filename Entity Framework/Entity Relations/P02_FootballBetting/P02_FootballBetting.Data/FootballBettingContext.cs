560e5f7df8c76167af9bc9c0c5b6e4068fd2983d
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-04-09 19:48:13 +0300

    Context warnings cleanup.

--- Entity Framework/Entity Relations/P02_FootballBetting/P02_FootballBetting.Data/FootballBettingContext.cs
+++ Entity Framework/Entity Relations/P02_FootballBetting/P02_FootballBetting.Data/FootballBettingContext.cs
@@ -11,25 +11,25 @@ public class FootballBettingContext : DbContext
 
     public FootballBettingContext(DbContextOptions options) : base(options) { }
 
-    public DbSet<Bet> Bets { get; set; }
+    public DbSet<Bet> Bets { get; set; } = null!;
 
-    public DbSet<Color> Colors { get; set; }
+    public DbSet<Color> Colors { get; set; } = null!;
 
-    public DbSet<Country> Countries { get; set; }
+    public DbSet<Country> Countries { get; set; } = null!;
 
-    public DbSet<Game> Games { get; set; }
+    public DbSet<Game> Games { get; set; } = null!;
 
-    public DbSet<Player> Players { get; set; }
+    public DbSet<Player> Players { get; set; } = null!;
 
-    public DbSet<PlayerStatistic> PlayersStatistics { get; set; }
+    public DbSet<PlayerStatistic> PlayersStatistics { get; set; } = null!;
 
-    public DbSet<Position> Positions { get; set; }
+    public DbSet<Position> Positions { get; set; } = null!;
 
-    public DbSet<Team> Teams { get; set; }
+    public DbSet<Team> Teams { get; set; } = null!;
 
-    public DbSet<Town> Towns { get; set; }
+    public DbSet<Town> Towns { get; set; } = null!;
 
-    public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
